26cf021e9a9b2bc35661c7b99d889f81a30131e8
maycon <mayconkabitschke@gmail.com>
2026-06-18 06:26:13 -0300

    menu mobile

--- src/components/Header/page.tsx
+++ src/components/Header/page.tsx
@@ -3,6 +3,7 @@ import { faGitAlt, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-i
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ArrowRight } from 'lucide-react';
 import { useState } from 'react';
+import { motion, AnimatePresence } from "framer-motion";
 
 
 
@@ -32,39 +33,73 @@ export const Header = () => {
                 <div></div>
             </div>
 
-            <div className={`${styles.sidebar} ${open ? styles.show : ""}`}>
-                <ul>
-                    <li><a href="#inicio" >Início</a></li>
-                    <li><a href="#projetos" >Projetos</a></li>
-                    <li><a href="#servicos">Serviços</a></li>
-                    <li><a href="#sobre">Sobre</a></li>
-                    <li><a href="#contatos">Contatos</a></li>
-                </ul>
+            <AnimatePresence>
+                {open && (
+                    <motion.div
+                        className={styles.overlay}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        onClick={() => setOpen(false)}
+                    >
+                        <motion.div
+                            className={styles.sidebar}
+                            initial={{ x: "-100%" }}
+                            animate={{ x: 0 }}
+                            exit={{ x: "-100%" }}
+                            transition={{
+                                type: "spring",
+                                stiffness: 300,
+                                damping: 30
+                            }}
+                            drag="x"
+                            dragConstraints={{ left: -300, right: 0 }}
+                            dragElastic={0.1}
+                            onClick={(e) => e.stopPropagation()}
+                            onDragEnd={(_, info) => {
+                                const shouldClose =
+                                    info.offset.x < -100 ||
+                                    info.velocity.x < -500;
 
+                                if (shouldClose) {
+                                    setOpen(false);
+                                }
+                            }}
+                        >
+                            <ul>
+                                <li><a href="#inicio">Início</a></li>
+                                <li><a href="#projetos">Projetos</a></li>
+                                <li><a href="#servicos">Serviços</a></li>
+                                <li><a href="#sobre">Sobre</a></li>
+                                <li><a href="#contatos">Contatos</a></li>
+                            </ul>
 
-                <div className={styles.contact}>
+                            <div className={styles.contact}>
+                                <div>
+                                    <a href="https://github.com/kabitschke" target="_blank">
+                                        <FontAwesomeIcon icon={faGitAlt} />
+                                    </a>
+                                </div>
 
+                                <div>
+                                    <a href="">
+                                        <FontAwesomeIcon icon={faLinkedin} />
+                                    </a>
+                                </div>
 
-                    <div>
-                        <a href="https://github.com/kabitschke" target="_blank">
-                            <FontAwesomeIcon icon={faGitAlt} />
-                        </a>
-                    </div>
-
-                    <div>
-                        <a href="">
-                            <FontAwesomeIcon icon={faLinkedin} />
-                        </a>
-                    </div>
-
-                    <div>
-                        <a href="http://api.whatsapp.com/send?phone=5527997145039" target='blank'>
-                            <FontAwesomeIcon icon={faWhatsapp} />
-                        </a>
-                    </div>
-
-                </div>
-            </div>
+                                <div>
+                                    <a
+                                        href="http://api.whatsapp.com/send?phone=5527997145039"
+                                        target="blank"
+                                    >
+                                        <FontAwesomeIcon icon={faWhatsapp} />
+                                    </a>
+                                </div>
+                            </div>
+                        </motion.div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
 
         </header>
     );
