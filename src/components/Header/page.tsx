f83e93d23a9523a2645c67d99d69bdc31672a136
Maycon Kabitschke <mayconkabitschke@gmail.com>
2026-06-20 15:11:38 -0300

    remove frame-motion

--- src/components/Header/page.tsx
+++ src/components/Header/page.tsx
@@ -3,7 +3,6 @@ import { faGitAlt, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-i
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ArrowRight } from 'lucide-react';
 import { useState } from 'react';
-import { motion, AnimatePresence } from "framer-motion";
 
 
 
@@ -17,6 +16,7 @@ export const Header = () => {
     return (
         <header className={styles.header}>
             <div className={styles.logo}>MK</div>
+
 
             <nav className={styles.nav}>
                 <ul>
@@ -37,73 +37,43 @@ export const Header = () => {
                 <div></div>
             </div>
 
-            <AnimatePresence>
-                {open && (
-                    <motion.div
-                        className={styles.overlay}
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                        onClick={() => setOpen(false)}
-                    >
-                        <motion.div
-                            className={styles.sidebar}
-                            initial={{ x: "-100%" }}
-                            animate={{ x: 0 }}
-                            exit={{ x: "-100%" }}
-                            transition={{
-                                type: "spring",
-                                stiffness: 300,
-                                damping: 30
-                            }}
-                            drag="x"
-                            dragConstraints={{ left: -300, right: 0 }}
-                            dragElastic={0.1}
-                            onClick={(e) => e.stopPropagation()}
-                            onDragEnd={(_, info) => {
-                                const shouldClose =
-                                    info.offset.x < -100 ||
-                                    info.velocity.x < -500;
+            <div
+                className={`${styles.overlay} ${open ? styles.show : ""}`}
+                onClick={() => setOpen(false)}
+            ></div>
 
-                                if (shouldClose) {
-                                    setOpen(false);
-                                }
-                            }}
+            <div className={`${styles.sidebar} ${open ? styles.show : ""}`}>
+                <ul onClick={handleClick}>
+                    <li><a href="#inicio" >Início</a></li>
+                    <li><a href="#projetos" >Projetos</a></li>
+                    <li><a href="#servicos" >Serviços</a></li>
+                    <li><a href="#sobre" >Sobre</a></li>
+                    <li><a href="#contatos" >Contatos</a></li>
+                </ul>
+
+                <div className={styles.contact}>
+                    <div>
+                        <a href="https://github.com/kabitschke" target="_blank">
+                            <FontAwesomeIcon icon={faGitAlt} />
+                        </a>
+                    </div>
+
+                    <div>
+                        <a href="">
+                            <FontAwesomeIcon icon={faLinkedin} />
+                        </a>
+                    </div>
+
+                    <div>
+                        <a
+                            href="http://api.whatsapp.com/send?phone=5527997145039"
+                            target="blank"
                         >
-                            <ul onClick={handleClick}>
-                                <li><a href="#inicio" >Início</a></li>
-                                <li><a href="#projetos" >Projetos</a></li>
-                                <li><a href="#servicos" >Serviços</a></li>
-                                <li><a href="#sobre" >Sobre</a></li>
-                                <li><a href="#contatos" >Contatos</a></li>
-                            </ul>
-
-                            <div className={styles.contact}>
-                                <div>
-                                    <a href="https://github.com/kabitschke" target="_blank">
-                                        <FontAwesomeIcon icon={faGitAlt} />
-                                    </a>
-                                </div>
-
-                                <div>
-                                    <a href="">
-                                        <FontAwesomeIcon icon={faLinkedin} />
-                                    </a>
-                                </div>
-
-                                <div>
-                                    <a
-                                        href="http://api.whatsapp.com/send?phone=5527997145039"
-                                        target="blank"
-                                    >
-                                        <FontAwesomeIcon icon={faWhatsapp} />
-                                    </a>
-                                </div>
-                            </div>
-                        </motion.div>
-                    </motion.div>
-                )}
-            </AnimatePresence>
+                            <FontAwesomeIcon icon={faWhatsapp} />
+                        </a>
+                    </div>
+                </div>
+            </div>
 
         </header>
     );
